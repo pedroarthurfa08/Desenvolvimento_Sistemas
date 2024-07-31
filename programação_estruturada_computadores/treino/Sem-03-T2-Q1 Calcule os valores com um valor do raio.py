bea74c3d5fcb30c291271fc371bc92b21ba7562a
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2024-07-31 20:33:28 -0300

    comit

---
+++ programação_estruturada_computadores/treino/Sem-03-T2-Q1 Calcule os valores com um valor do raio.py
@@ -0,0 +1,11 @@
+import math
+pi = 3.14
+raio = float(input())
+cirncuferencia = 2 * pi * raio
+print(cirncuferencia)
+areacirculo = raio ** 2 * raio
+print(areacirculo)
+areaesfera = raio ** 2 * pi * 4
+print(areaesfera)
+volumesferea = (raio ** 3 * pi * 4)/3
+print(volumesferea)
